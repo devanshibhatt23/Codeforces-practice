9f6c7d13faa54b67fbaefef2c6920c1670fb06ad
Devanshi Bhatt <dbhatt2310@gmail.com>
2026-06-25 19:18:03 +0530

    Seating arrangement - easy version 1300 div2 C

---
+++ 1300/03_seating_arrangement_easy.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i,s,e) for(int i=s; i<e; i++)
+#define en cout << ans << "\n"
+#define nn cout << "\n"
+#define vec vector<ll> 
+#define ci cin >> v[i];
+#define s(v) sort(v.begin(), v.end())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define re return;
+using namespace std;
+
+ll x,s;
+
+int func(int idx, string a) {
+    int tables = 0, ans = 0;
+
+    for(char ch : a) {
+        if(ch == 'I') {
+            if(tables < x) {
+                tables++;
+                ans++;
+            }
+        }
+        else if(ch == 'E') {
+            if(ans < tables*s) ans++;
+        }
+        else {
+            if(idx > 0) {
+                if(tables < x) {
+                    tables++;
+                    ans++;
+                }
+                idx--;
+            }
+            else {
+                if(ans < tables*s) ans++;
+            }
+        }
+    }
+
+    return ans;
+}
+
+void solve() {
+    int n;
+    cin >> n >> x >> s;
+
+    string a;
+    cin >> a;
+
+    int ct_a = 0;
+    int ans = INT_MIN;
+
+    for(char ch : a) if(ch == 'A') ct_a++;
+
+    f(i,0,ct_a+1) ans = max(ans,func(i,a));
+
+    en;
+}
+ 
+int main() {
+    int t = 1;
+    cin >> t;
+    
+    while(t--) {
+        solve();
+    }
+}
